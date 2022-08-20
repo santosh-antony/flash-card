9bcd27f811463b385254fbb3e4a7b349a47b82f9
santosh-antony <visanty@gmail.com>
2022-08-20 15:35:37 +0530

    Completed Show box, list boxes & pick box randomly

--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,7 @@
 # cards/views.py
 
+import random
+
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -20,3 +22,16 @@ class CardCreateView(CreateView):
 
 class CardUpdateView(CardCreateView, UpdateView):
     success_url = reverse_lazy("card-list")
+
+class BoxView(CardListView):
+    template_name = "cards/box.html"
+
+    def get_queryset(self):
+        return Card.objects.filter(box=self.kwargs["box_num"])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["box_number"] = self.kwargs["box_num"]
+        if self.object_list:
+            context["check_card"] = random.choice(self.object_list)
+        return context
