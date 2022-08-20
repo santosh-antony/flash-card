ec5e33f753b918b12f6c9f10ac8747c7e0ea1dd3
santosh-antony <visanty@gmail.com>
2022-08-20 09:24:03 +0530

    successfully listed all cards on flashcard app

--- cards/urls.py
+++ cards/urls.py
@@ -1,12 +1,14 @@
 # cards/urls.py
 
 from django.urls import URLPattern, path
-from django.views.generic import TemplateView
+# Removed: from django.views.generic import TemplateView
+
+from . import views
 
 urlpatterns = [
     path(
         '',
-        TemplateView.as_view(template_name='cards/base.html'),
-        name='home'
+        views.CardListView.as_view(),
+        name='card-list'
     ),
 ]
